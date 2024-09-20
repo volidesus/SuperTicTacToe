60fe19e0b3359e22a25e3b9a5079b76d9b2f4437
volidesus <arkananta.arslan@gmail.com>
2024-09-20 17:49:48 +0700

    Edited CMakeLists.txt (Switched to vcpkg) and gitignore (Ignore build)

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/LENOVO/Documents/cppPortofolio/superTicTacToe
+# Install script for directory: C:/Users/LENOVO/Documents/cppPortofolio/SuperTicTacToe
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -47,6 +47,6 @@ endif()
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-  file(WRITE "C:/Users/LENOVO/Documents/cppPortofolio/superTicTacToe/build/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "C:/Users/LENOVO/Documents/cppPortofolio/SuperTicTacToe/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
